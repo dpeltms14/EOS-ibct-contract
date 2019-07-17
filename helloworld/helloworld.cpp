bd933ee42077b57194e2e72e688eab0d08f12e83
dpeltms14 <dpeltms14@naver.com>
2019-07-17 14:03:30 +0900

    2019-07-17_helloworld

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,17 @@
+#include <eosiolib/eosio.hpp>
+
+using namespace eosio;
+
+CONTRACT helloworld: public contract {
+    public:
+        using contract::contract;
+
+        ACTION hi(name user) {
+            check(has_auth(user), "missing authority");
+            //require_auth(user);
+
+            print("hello, ", user);
+        }
+
+    private:
+};
